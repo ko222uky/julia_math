43e1b35b988441bda619a55453c7ee6a0b59ab75
Kenneth O'Dell <kenneth.l.odell@gmail.com>
2024-12-18 22:46:00 -0500

    Fixed test file.

--- tests/test.jl
+++ tests/test.jl
@@ -1,12 +1,13 @@
 using Plots
-using Interact
 
-# Define the polynomial function
+# define the two Gaussian parameters
 c = 50
 σ = 20
-f(x, c, σ) = exp((-1/2)*((x - c)/σ)^2) 
 
-# Create the x values (domain)
+# Gaussian function
+f(x) = exp((-1/2)*((x - c)/σ)^2) 
+
+# Create the x values with step size of 0.1 from 0 to 100.
 x = 0:0.1:100  
 
 
@@ -17,6 +18,10 @@ plot(x,
     ylabel="f(x)", 
     title="Gaussian Function (c = 50, σ = 20)", 
     lw=2,
-    legend=:outertopright
+    legend=:outertop
 )
 gui()
+
+# Run with $julia test.jl
+# You can also run by running $julia to open the REPL
+# #... then enter include("test.jl")
